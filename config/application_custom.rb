dd7f769c9b19038fe7570a10a9ba228d6229e4c8
Alejandro Rodriguez <alejandro.rodriguez@enreda.coop>
2018-07-27 12:44:42 +0200

    Custom residence

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,7 +1,7 @@
 module Consul
   class Application < Rails::Application
   	config.i18n.default_locale = :es
-  	config.i18n.available_locales = [:es,:gl,:en] 
+  	config.i18n.available_locales = [:es,:en] 
   	require Rails.root.join('lib/custom/census_api')
   end
 
