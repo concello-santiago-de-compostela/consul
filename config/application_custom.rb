c78034b1fbd1c484848b8421b6e76eb4b1f3a728
Alejandro Rodriguez <alejandro.rodriguez@enreda.coop>
2018-07-27 11:43:36 +0200

    Add path api_censo

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -2,7 +2,7 @@ module Consul
   class Application < Rails::Application
   	config.i18n.default_locale = :es
   	config.i18n.available_locales = [:es,:gl,:en] 
-  	
+  	require Rails.root.join('lib/custom/census_api')
   end
 
 end
