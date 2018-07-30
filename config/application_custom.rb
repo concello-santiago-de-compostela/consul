78a71f2cd3499eecd4433efc17599e9b133c70c0
Alejandro Rodriguez <alejandro.rodriguez@enreda.coop>
2018-07-30 12:58:10 +0200

    New language GL

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,7 +1,7 @@
 module Consul
   class Application < Rails::Application
-  	config.i18n.default_locale = :gl
-  	config.i18n.available_locales = [:gl, :es, :en] 
+  	config.i18n.default_locale = 'gl-ES'
+  	config.i18n.available_locales = ['gl-ES', :es, :en] 
   	require Rails.root.join('lib/custom/census_api')
   end
 
